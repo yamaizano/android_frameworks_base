b8380c33f06b286fe302560ea464b3aafd30f231
Danesh M <daneshm90@gmail.com>
2014-11-10 16:37:14 -0800

    Make people lookup default overlay
    Change-Id: I6dd7267bc82d33e04d99024e4dfb193f071d4f97

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2154,6 +2154,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             loadIntegerSetting(stmt, Settings.System.SCREEN_ANIMATION_STYLE,
                     R.integer.def_screen_animation_style);
 
+            loadIntegerSetting(stmt, Settings.System.ENABLE_PEOPLE_LOOKUP,
+                    R.integer.def_people_lookup);
+
             loadDefaultAnimationSettings(stmt);
 
             loadRibbonSetting(stmt);
