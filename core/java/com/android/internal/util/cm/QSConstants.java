5be32b112b7660f2d9e8835660e27c80bf06274c
Steve Kondik <steve@cyngn.com>
2015-02-18 03:52:24 -0800

    livedisplay: Implement active display adjustment
     * LiveDisplay is our new name for the various display technologies
       which adjust the screen based on environmental conditions other
       than the standard automatic backlight controls.
     * This patch implements automatic color temperature adjustment based
       on time of day. This is similar to f.lux or Redshift. My eyes are
       so happy now!
     * Automatic outdoor/SRE feature is now handled here.
     * Handling of CABC/CABL and color enhancements also handled here.
     * Manual RGB tweaking is handled here.
     * Can delegate to DisplayColor HAL if available, otherwise uses
       SurfaceFlinger's 1015 operation to apply changes.
     * Happily coexists with the new accessibility features for color
       blindness correction and color inversion.
     * All postprocessing will be disabled when powersave mode is
       activated.
    
    Change-Id: Iac1b74f410957f8e2d8290465c4ce9cc1fd97a88
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -49,6 +49,7 @@ public class QSConstants {
     public static final String TILE_POWER_MENU = "power_menu";
     public static final String TILE_BATTERY_SAVER = "battery_saver";
     public static final String TILE_BRIGHTNESS = "brightness";
+    public static final String TILE_LIVE_DISPLAY = "live_display";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -88,5 +89,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_POWER_MENU);
         TILES_AVAILABLE.add(TILE_BATTERY_SAVER);
         TILES_AVAILABLE.add(TILE_BRIGHTNESS);
+        TILES_AVAILABLE.add(TILE_LIVE_DISPLAY);
     }
 }
