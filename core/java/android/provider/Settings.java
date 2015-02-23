75d78fdb9d6999ac361def19faba43ee5f9e9634
Michael Bestas <mikeioannina@gmail.com>
2015-02-23 20:44:27 +0200

    Cleanup deprecated settings
    Change-Id: I2a75f5bece7694cf11c00fcf00ab507eda547893
    
    @alviteri: Follow the same steps for our own features
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2180,40 +2180,45 @@ public final class Settings {
         /**
          * The keyboard brightness to be used while the screen is on.
          * Valid value range is between 0 and {@link PowerManager#getMaximumKeyboardBrightness()}
-         * @deprecated
+         * @deprecated Use {@link android.provider.Settings.Secure#KEYBOARD_BRIGHTNESS} instead
          * @hide
          */
+        @Deprecated
         public static final String KEYBOARD_BRIGHTNESS = Secure.KEYBOARD_BRIGHTNESS;
 
         /**
          * The button brightness to be used while the screen is on or after a button press,
          * depending on the value of {@link BUTTON_BACKLIGHT_TIMEOUT}.
          * Valid value range is between 0 and {@link PowerManager#getMaximumButtonBrightness()}
-         * @deprecated
+         * @deprecated Use {@link android.provider.Settings.Secure#BUTTON_BRIGHTNESS} instead
          * @hide
          */
+        @Deprecated
         public static final String BUTTON_BRIGHTNESS = Secure.BUTTON_BRIGHTNESS;
 
         /**
          * The time in ms to keep the button backlight on after pressing a button.
          * A value of 0 will keep the buttons on for as long as the screen is on.
-         * @deprecated
+         * @deprecated Use {@link android.provider.Settings.Secure#BUTTON_BACKLIGHT_TIMEOUT} instead
          * @hide
          */
+        @Deprecated
         public static final String BUTTON_BACKLIGHT_TIMEOUT = Secure.BUTTON_BACKLIGHT_TIMEOUT;
 
-        /** Whether to allow one finger quick settings expansion on the right side of the statusbar.
-         *
+        /**
+         * Whether to allow one finger quick settings expansion on the right side of the statusbar.
          * @hide
          */
         public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
 
-        /** Whether to show the brightness slider in quick settings panel.
-         *
+        /**
+         * Whether to show the brightness slider in quick settings panel.
+         * @deprecated Use {@link android.provider.Settings.Secure#QS_SHOW_BRIGHTNESS_SLIDER}
+         * instead
          * @hide
          */
         @Deprecated
-        public static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
+        public static final String QS_SHOW_BRIGHTNESS_SLIDER = Secure.QS_SHOW_BRIGHTNESS_SLIDER;
 
         /**
          * Quick Settings Smart Pulldown
@@ -2257,18 +2262,21 @@ public final class Settings {
         public static final String BATTERY_SAVER_MODE_COLOR = "battery_save_mode_color";
 
         /**
-         * Deprecated Use {@link android.provider.Settings.Secure.QS_TILES}
+         * List of QS tile names
+         * @deprecated Use {@link android.provider.Settings.Secure#QS_TILES} instead
          * @hide
          */
         @Deprecated
-        public static final String QS_TILES = "sysui_qs_tiles";
+        public static final String QS_TILES = Secure.QS_TILES;
 
         /**
-         * Deprecated Use {@link android.provider.Settings.Secure.QS_USE_MAIN_TILES}
+         * Use "main" tiles on the first row of the quick settings panel
+         * 0 = no, 1 = yes
+         * @deprecated Use {@link android.provider.Settings.Secure#QS_USE_MAIN_TILES} instead
          * @hide
          */
         @Deprecated
-        public static final String QS_USE_MAIN_TILES = "sysui_qs_main_tiles";
+        public static final String QS_USE_MAIN_TILES = Secure.QS_USE_MAIN_TILES;
 
         /**
          * Control whether the process CPU usage meter should be shown.
@@ -2688,37 +2696,42 @@ public final class Settings {
 
         /**
          * Wether navigation bar is enabled or not
-         * @deprecated
+         * @deprecated Use {@link android.provider.Settings.Secure#NAVIGATION_BAR_SHOW} instead
          * @hide
          */
+        @Deprecated
         public static final String NAVIGATION_BAR_SHOW = Secure.NAVIGATION_BAR_SHOW;
 
         /**
          * Navigation bar height when it is on protrait
-         * @deprecated
+         * @deprecated Use {@link android.provider.Settings.Secure#NAVIGATION_BAR_HEIGHT} instead
          * @hide
          */
+        @Deprecated
         public static final String NAVIGATION_BAR_HEIGHT = Secure.NAVIGATION_BAR_HEIGHT;
 
         /**
          * Navigation bar height when it is on landscape at the right
-         * @deprecated
+         * @deprecated Use {@link android.provider.Settings.Secure#NAVIGATION_BAR_WIDTH} instead
          * @hide
          */
+        @Deprecated
         public static final String NAVIGATION_BAR_WIDTH = Secure.NAVIGATION_BAR_WIDTH;
 
         /**
          * Navigation bar button color
-         * @deprecated
+         * @deprecated Use {@link android.provider.Settings.Secure#NAVIGATION_BAR_TINT} instead
          * @hide
          */
+        @Deprecated
         public static final String NAVIGATION_BAR_TINT = Secure.NAVIGATION_BAR_TINT;
 
         /**
          * Enable HW keys
-         * @deprecated
+         * @deprecated Use {@link android.provider.Settings.Secure#ENABLE_HW_KEYS} instead
          * @hide
          */
+        @Deprecated
         public static final String ENABLE_HW_KEYS = Secure.ENABLE_HW_KEYS;
 
         /**
@@ -6278,8 +6291,8 @@ public final class Settings {
          */
         public static final String APP_PERFORMANCE_PROFILES_ENABLED = "app_perf_profiles_enabled";
 
-        /** Whether to show the brightness slider in quick settings panel.
-         *
+        /**
+         * Whether to show the brightness slider in quick settings panel.
          * @hide
          */
         public static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
