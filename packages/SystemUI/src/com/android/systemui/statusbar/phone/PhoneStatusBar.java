31974bf365c0f6649683cc594fe4af684f34a990
Danny Baumann <dannybaumann@web.de>
2015-03-06 11:23:05 +0100

    Clean up battery handling.
    Use a single observer for all battery meter and level views, and remove
    no-longer used callback registrations.
    
    Change-Id: I8f9db5e484628d7fbc74fe892d64bef5f08cea24

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1074,7 +1074,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         // Other icons
         mLocationController = new LocationControllerImpl(mContext); // will post a notification
-        mBatteryController = new BatteryController(mContext);
+        mBatteryController = new BatteryController(mContext, mHandler);
         mBatteryController.addStateChangedCallback(new BatteryStateChangeCallback() {
             @Override
             public void onPowerSaveChanged() {
@@ -1085,6 +1085,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
             @Override
             public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
+                // noop
+            }
+            @Override
+            public void onBatteryStyleChanged(int style, int percentMode) {
                 // noop
             }
         });
@@ -3762,6 +3766,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         if (mMSimNetworkController != null) {
             mMSimNetworkController.setUserId(mCurrentUserId);
+        }
+        if (mBatteryController != null) {
+            mBatteryController.setUserId(mCurrentUserId);
         }
     }
 
