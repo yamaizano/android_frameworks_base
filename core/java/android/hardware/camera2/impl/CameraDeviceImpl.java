yamaizano/android_frameworks_base

commit 232d28f3a3e7a3a710470fa8f114ab7385bfaa63

Add config overlay to force enable multi resolution for camera
* Apparently, required by OOS camera port.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/android/hardware/camera2/impl/CameraDeviceImpl.java
@@ -129,6 +129,8 @@ public class CameraDeviceImpl extends CameraDevice
     private final int mTotalPartialCount;
     private final Context mContext;
 
+    private final boolean mForceMultiResolution;
+
     private static final long NANO_PER_SECOND = 1000000000; //ns
 
     /**
@@ -302,6 +304,9 @@ public class CameraDeviceImpl extends CameraDevice
             mTotalPartialCount = partialCount;
         }
         mIsPrivilegedApp = checkPrivilegedAppList();
+
+        mForceMultiResolution = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_forceMultiResolution);
     }
 
     public CameraDeviceCallbacks getCallbacks() {
@@ -493,7 +498,7 @@ public class CameraDeviceImpl extends CameraDevice
                     if (inputConfig != null) {
                         int streamId = mRemoteDevice.createInputStream(inputConfig.getWidth(),
                                 inputConfig.getHeight(), inputConfig.getFormat(),
-                                inputConfig.isMultiResolution());
+                                inputConfig.isMultiResolution() || mForceMultiResolution);
                         mConfiguredInput = new SimpleEntry<Integer, InputConfiguration>(
                                 streamId, inputConfig);
                     }
@@ -1525,7 +1530,7 @@ public class CameraDeviceImpl extends CameraDevice
             return;
         }
         int inputFormat = inputConfig.getFormat();
-        if (inputConfig.isMultiResolution()) {
+        if (inputConfig.isMultiResolution() || mForceMultiResolution) {
             MultiResolutionStreamConfigurationMap configMap = mCharacteristics.get(
                     CameraCharacteristics.SCALER_MULTI_RESOLUTION_STREAM_CONFIGURATION_MAP);
 

core/res/res/values/cr_config.xml
@@ -71,4 +71,7 @@
     <!-- Defines custom OEM sensor for pocket detection. -->
     <string name="config_pocketJudgeVendorSensorName"></string>
     <string name="config_pocketJudgeVendorProximitySensorName"></string>
+
+    <!-- Whether to force enable multi resolution configuration for camera -->
+    <bool name="config_forceMultiResolution">false</bool>
 </resources>

core/res/res/values/cr_symbols.xml
@@ -55,4 +55,7 @@
     <!-- Pocket Service -->
     <java-symbol type="string" name="config_pocketJudgeVendorSensorName" />
     <java-symbol type="string" name="config_pocketJudgeVendorProximitySensorName" />
+
+    <!-- Whether to force enable multi resolution configuration for camera -->
+    <java-symbol type="bool" name="config_forceMultiResolution" />
 </resources>

commit 10de82c6ba7b6540a9536180b9edd92814e14cd1

fixup! Add config overlay to force enable multi resolution for camera
* Fixes: 232d28f3a3e7a3a710470fa8f114ab7385bfaa63

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/android/hardware/camera2/impl/CameraDeviceImpl.java
@@ -498,7 +498,7 @@ public class CameraDeviceImpl extends CameraDevice
                     if (inputConfig != null) {
                         int streamId = mRemoteDevice.createInputStream(inputConfig.getWidth(),
                                 inputConfig.getHeight(), inputConfig.getFormat(),
-                                inputConfig.isMultiResolution() || mForceMultiResolution);
+                                inputConfig.isMultiResolution());
                         mConfiguredInput = new SimpleEntry<Integer, InputConfiguration>(
                                 streamId, inputConfig);
                     }