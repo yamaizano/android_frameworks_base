3ad1679f13d91a42756ffdc7fe70f8b0ba4e31e6
Yukizaki Miyamura <yukizakimiyamura@gmail.com>
2025-04-18 07:27:36 +0700

    keystore: This way a user can use PIF
    * ref: https://github.com/ximi-libra-test/libra_manifests/blob/master/crdroid-security/0010-revert-gms-key-attestation-block.diff
    
    Signed-off-by: Yukizaki Miyamura <yukizakimiyamura@gmail.com>

--- keystore/java/android/security/KeyStore.java
+++ keystore/java/android/security/KeyStore.java
@@ -1128,11 +1128,6 @@ public class KeyStore {
 
     public int attestKey(
             String alias, KeymasterArguments params, KeymasterCertificateChain outChain) {
-        // Prevent Google Play Services from using key attestation for SafetyNet
-        if (mContext.getPackageName().equals("com.google.android.gms")) {
-            return KeymasterDefs.KM_ERROR_KEY_RATE_LIMIT_EXCEEDED;
-        }
-
         CertificateChainPromise promise = new CertificateChainPromise();
         try {
             mBinder.asBinder().linkToDeath(promise, 0);
